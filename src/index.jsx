4a39ea9fd60149a37a016b08c98ca856d5fa2492
Vinicius José <viniciusjosedev@gmail.com>
2023-05-13 10:53:21 -0300

    more updates

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import WidthProvider from './context/WidthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
 	  <WidthProvider>
       <App />
 		</WidthProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
